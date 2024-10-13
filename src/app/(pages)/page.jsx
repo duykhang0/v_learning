5542883fe61c516271834f94bef05bb8e3078526
dykhang <duykhang071120@gmail.com>
2024-10-13 14:41:59 +0700

    add fontawesome

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,8 +1,9 @@
 
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 export default function Home() {
   return (
    <div className="home fs-2">
-      home
+    home
    </div>
   );
 }
