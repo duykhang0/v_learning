aa88329ae4656fc77605a80dd5b1dc90edb50dda
dykhang <duykhang071120@gmail.com>
2024-11-28 23:37:49 +0700

    update nextjs congif

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    reactStrictMode: true,
-    images: {
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: 'i.pinimg.com',
-                pathname: '/static/media/**',
-            }
-        ]
-    }
+    
 };
 
 export default nextConfig;
